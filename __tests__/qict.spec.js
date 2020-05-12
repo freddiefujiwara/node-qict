d75e43965540d7159daf5dcb5de63ddd6464c81f
Fumikazu Fujiwara <github@ze.gs>
2020-05-12 17:54:58 +0900

    add clean

--- __tests__/qict.spec.js
+++ __tests__/qict.spec.js
@@ -7,19 +7,28 @@ describe('Qict', () => {
     expect(q).toBeInstanceOf(Qict);
   });
   it('can clean all parameters', () => {
-    const q = new Qict('testData.txt');
+    const q = new Qict('__tests__/testData.txt');
     expect(q.parameters.length).toBe(0);
-    expect(q.numberParameters).toBe(0);
     expect(q.parameterValues.length).toBe(0);
-    expect(q.numberParameterValues).toBe(0);
+    expect(q.parameterPositions.length).toBe(0);
     expect(q.legalValues.length).toBe(0);
+    expect(q.unusedPairs.length).toBe(0);
+    expect(q.numberParameters).toBe(0);
+    expect(q.numberParameterValues).toBe(0);
     expect(q.numberPairs).toBe(0);
+
     expect(q.clean).toBeInstanceOf(Function);
+    q.readFile();
+    q.extractParameters();
+    q.clean();
+
     expect(q.parameters.length).toBe(0);
-    expect(q.numberParameters).toBe(0);
     expect(q.parameterValues.length).toBe(0);
-    expect(q.numberParameterValues).toBe(0);
+    expect(q.parameterPositions.length).toBe(0);
     expect(q.legalValues.length).toBe(0);
+    expect(q.unusedPairs.length).toBe(0);
+    expect(q.numberParameters).toBe(0);
+    expect(q.numberParameterValues).toBe(0);
     expect(q.numberPairs).toBe(0);
   });
   it('can read all strings from file', () => {
@@ -40,13 +49,16 @@ describe('Qict', () => {
     expect(q.extractParameters).toBeInstanceOf(Function);
     q.extractParameters();
     expect(q.parameters.length).toBe(4);
-    expect(q.numberParameters).toBe(4);
     expect(q.parameterValues.length).toBe(11);
-    expect(q.numberParameterValues).toBe(11);
+    expect(q.parameterPositions.length).toBe(11);
     expect(q.legalValues.length).toBe(4);
     expect(q.legalValues[0].length).toBe(2);
     expect(q.legalValues[1].length).toBe(4);
     expect(q.legalValues[2].length).toBe(3);
     expect(q.legalValues[3].length).toBe(2);
+    expect(q.unusedPairs.length).toBe(44);
+    expect(q.numberParameters).toBe(4);
+    expect(q.numberParameterValues).toBe(11);
+    expect(q.numberPairs).toBe(44);
   });
 });
--- src/qict.js
+++ src/qict.js
@@ -9,30 +9,52 @@ class Qict {
   }
   extractParameters(){
     this.clean();
+    //readlines
     this.contents.split(/\r\n/).forEach((line) => {
+      //create pairs parameters: values
       const pair = line.split(/:/);
-      if(pair.length > 1){
-        let values = new Array();
-        pair[1].split(/,/).forEach((value) => {
-          this.parameterValues.push(value.trim());
-          values.push(value.trim());
-          this.numberParameterValues++;
-        });
-        this.legalValues.push(values)
+      //should be more than 2
+      if(pair.length < 2){
+        return;
       }
       this.parameters.push(pair[0]);
+
+      //values analysis
+      let values = new Array();
+      pair[1].split(/,/).forEach((value) => {
+        values.push(this.numberParameterValues);
+        this.parameterValues.push(value.trim());
+        this.numberParameterValues++;
+      });
+      this.legalValues.push(values)
       this.numberParameters ++;
     });
     for (let i = 0; i <= this.legalValues.length - 2; ++i){
       for (let j = i + 1; j <= this.legalValues.length - 1; ++j){
         this.numberPairs += (this.legalValues[i].length * this.legalValues[j].length);
+        for (let x = 0; x < this.legalValues[i].length; ++x) {
+          for (let y = 0; y < this.legalValues[j].length; ++y) {
+            let pair = new Array();
+            pair.push(this.legalValues[i][x]);
+            pair.push(this.legalValues[j][y]);
+            this.unusedPairs.push(pair);
+          }
+        }
+      }
+    }
+    let k = 0;
+    for (let i = 0; i < this.legalValues.length; ++i) {
+      for (let j = 0; j < this.legalValues[i].length; ++j) {
+        this.parameterPositions[k++] = i;
       }
     }
   }
   clean(){
     this.parameters = new Array();
     this.parameterValues = new Array();
+    this.parameterPositions = new Array();
     this.legalValues = new Array();
+    this.unusedPairs = new Array();
     this.numberParameters = 0;
     this.numberParameterValues = 0;
     this.numberPairs = 0;
